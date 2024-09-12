8cb68bca1f8c7298780a4f3b2c56d790dba857d2
Nicole <nicolerodriguezluna@estudiantec.cr>
2024-09-11 19:24:39 -0600

    Change backend route

--- frontend/src/scripts/constants.js
+++ frontend/src/scripts/constants.js
@@ -1,3 +1,7 @@
-export const BACKEND_ROUTE = process.env.REACT_APP_BACKEND_ROUTE;
+const isProduction = process.env.NODE_ENV === 'production';
+
+export const BACKEND_ROUTE = isProduction
+  ? "https://comiteseguridad-backend.onrender.com"
+  : "http://localhost:5000";
 export const MAX_PRODUCT_AMOUNT = 5;
 export const DELIVERY_FEE = 3000;
